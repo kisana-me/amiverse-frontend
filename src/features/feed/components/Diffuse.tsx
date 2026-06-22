97ec08a7b68fb71d999405d8b700f984d6f3612f
kisana <kisana@amiverse.net>
2026-06-22 23:05:55 +0900

    diffuse小修整

--- src/features/feed/components/Diffuse.tsx
+++ src/features/feed/components/Diffuse.tsx
@@ -10,15 +10,12 @@ export default function Diffuse({ diffuse }: { diffuse: FeedItemType | undefined
 
   return (
     <div className={styles.diffuse}>
-      <img src={diffuse.account.icon_url || '/ast-imgs/icon.png'} alt="" style={{ width: '24px', height: '24px', borderRadius: '50%', objectFit: 'cover' }} />
-      <span style={{ fontWeight: 'bold' }}>
-        <Link prefetch={false} href={`/@${diffuse.account.name_id}`}>
-          {diffuse.account.name}
-        </Link>
-      </span>
+      <img src={diffuse.account.icon_url || '/ast-imgs/icon.png'} alt="" className={styles.icon} />
+      <Link prefetch={false} href={`/@${diffuse.account.name_id}`} style={{ fontWeight: 'bold' }}>
+        {diffuse.account.name}
+      </Link>
       <span>@{diffuse.account.name_id}</span>
-
-      <div style={{ marginLeft: 'auto' }}>
+      <div>
         {diffuse.created_at && formatRelativeTime(new Date(diffuse.created_at))} 拡散
       </div>
     </div>
