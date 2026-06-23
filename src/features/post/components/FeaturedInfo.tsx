d951e2a2417637b8f0f564ec7ab12fee870ed4b5
kisana <kisana@amiverse.net>
2026-06-23 16:34:10 +0900

    feat/post 数字系表示変更

--- src/features/post/components/FeaturedInfo.tsx
+++ src/features/post/components/FeaturedInfo.tsx
@@ -8,16 +8,16 @@ export default function FeaturedInfo({ post }: { post: PostType }) {
   return (
     <div className={styles.info}>
       <Link prefetch={false} href={'/posts/' + post.aid + '/quotes'} className={styles.link}>
-        引用数: {post.quotes_count || 0}
+        <span className={styles.number}>{post.quotes_count || 0}</span> 引用
       </Link>
       <Link prefetch={false} href={'/posts/' + post.aid + '/diffusions'} className={styles.link}>
-        拡散数: {post.diffuses_count || 0}
+        <span className={styles.number}>{post.diffuses_count || 0}</span> 拡散
       </Link>
       <Link prefetch={false} href={'/posts/' + post.aid + '/reactions'} className={styles.link}>
-        リアクション数: {post.reactions_count || 0}
+        <span className={styles.number}>{post.reactions_count || 0}</span> リアクション
       </Link>
       <div>
-        閲覧数: {post.views_count || 0}
+        <span className={styles.number}>{post.views_count || 0}</span> 閲覧
       </div>
     </div>
   )
